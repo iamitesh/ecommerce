6603f161d4988f5e6a9c039335703a53ad51f8a0
Amitesh Anand <amitesh_anand@epam.com>
2025-03-15 20:30:48 +0530

    remove unused

--- app/fetch-test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function FetchTest() {
-  const response = await fetch('http://localhost:3000/api/hello');
-  const data = await response.json(); // ✅ Await JSON parsing
-
-  console.log('response', data);
-
-  return (<h1 className="text-4xl font-bold mb-8">{data.message}</h1>);
-}
