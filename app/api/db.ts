12c50f9d1d0c6b09e13283f6e45c6758d0089824
Amitesh Anand <amitesh_anand@epam.com>
2025-03-15 14:59:05 +0530

    connect to db

--- app/api/db.ts
+++ app/api/db.ts
@@ -8,8 +8,8 @@ export async function connectToDb() {
     return { client: cachedClient, db: cachedDb };
   }
 
-  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.vqdwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+  const uri = `mongodb+srv://aamiteshdev:gRiDKIbTXenxSuWJ@cluster0.og9fy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
   const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
